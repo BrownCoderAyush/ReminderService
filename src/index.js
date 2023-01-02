305ffb99aa775e8ab95da6e72a0537c0665dcced
Ayush <ayushchopra0@gmail.com>
2023-01-02 23:49:32 +0530

    basic setu[

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {PORT , DB_SYNC} = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+// const db = require('./models/index');
+
+const app = express();
+
+const setupAndStartServer = ()=>{
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use('/api' , apiRoutes);
+
+    app.listen(PORT , ()=>{
+        console.log(`Server started running on port ${PORT}`);
+        // if(DB_SYNC){
+        //     db.sequelize.sync({alter:true});
+        // }
+    })
+}
+
+
+setupAndStartServer();
